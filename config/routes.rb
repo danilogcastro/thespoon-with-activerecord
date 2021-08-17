3e09df5f39c32501cb7e318783d3f4a81f417371
Danilo Castro <danilogcastro@hotmail.com>
2021-08-17 10:08:42 -0300

    index, show, new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb '/path', to: 'controller#action'
+  # READ
+  # READ ALL RESTAURANTS
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new'
+  get '/restaurants/:id', to: 'restaurants#show'
+  # CREATE
+  post '/restaurants', to: 'restaurants#create'
+  # UPDATE
+  # DELETE
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding..."
+
+50.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+end
+
+puts "Done!"
